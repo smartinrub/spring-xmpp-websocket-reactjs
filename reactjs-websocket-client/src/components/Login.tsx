878de9ce73ff8ddac4ca4f4411a00b8b311cc2ac
Sergio Martin Rubio <econsergio@gmail.com>
2020-02-08 12:37:54 +0000

    Extract component

---
+++ reactjs-websocket-client/src/components/Login.tsx
@@ -0,0 +1,46 @@
+import React, { useState, FC } from 'react';
+import { Button, FormControl, FormLabel, Form } from 'react-bootstrap';
+
+export type LoginProps = {
+  wsConnect: (email: string) => void;
+};
+
+const Login: FC<LoginProps> = ({ wsConnect }) => {
+  const [username, setUsername] = useState('');
+
+  const validateForm = () => {
+    return username.length > 0;
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    wsConnect(username);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} className="form-signin">
+      <FormLabel className="sr-only">Username</FormLabel>
+      <FormControl
+        autoFocus
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(e: any) => setUsername(e.target.value)}
+        required
+      />
+      <div className="invalid-feedback" style={{ width: '100%' }}>
+        Your username is required.
+      </div>
+      <Button
+        className="btn btn-lg btn-primary btn-block"
+        block
+        disabled={!validateForm()}
+        type="submit"
+      >
+        Start Chat
+      </Button>
+    </Form>
+  );
+};
+
+export default Login;
