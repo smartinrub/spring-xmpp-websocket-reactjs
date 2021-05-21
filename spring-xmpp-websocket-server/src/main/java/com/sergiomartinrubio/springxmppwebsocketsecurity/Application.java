ac8af714dd82891175605227509bb8e4f16a7751
Sergio Martin Rubio <econsergio@gmail.com>
2021-05-21 08:29:34 +0200

    Rename application class

--- spring-xmpp-websocket-server/src/main/java/com/sergiomartinrubio/springxmppwebsocketsecurity/Application.java
+++ spring-xmpp-websocket-server/src/main/java/com/sergiomartinrubio/springxmppwebsocketsecurity/Application.java
@@ -7,10 +7,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties(XMPPConnection.class)
-public class SpringXmppWebsocketSecurityApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringXmppWebsocketSecurityApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
