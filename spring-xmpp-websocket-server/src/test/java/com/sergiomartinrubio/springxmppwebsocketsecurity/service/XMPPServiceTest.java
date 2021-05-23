7a13456e0fffbc8bf7b43c007f8341583da054a7
Sergio Martin Rubio <econsergio@gmail.com>
2021-05-23 16:04:56 +0200

    Add test

--- spring-xmpp-websocket-server/src/test/java/com/sergiomartinrubio/springxmppwebsocketsecurity/service/XMPPServiceTest.java
+++ spring-xmpp-websocket-server/src/test/java/com/sergiomartinrubio/springxmppwebsocketsecurity/service/XMPPServiceTest.java
@@ -19,6 +19,7 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 import javax.websocket.Session;
 import java.util.Optional;
 
+import static com.sergiomartinrubio.springxmppwebsocketsecurity.model.MessageType.ERROR;
 import static com.sergiomartinrubio.springxmppwebsocketsecurity.model.MessageType.FORBIDDEN;
 import static com.sergiomartinrubio.springxmppwebsocketsecurity.model.MessageType.JOIN_SUCCESS;
 import static org.mockito.BDDMockito.given;
@@ -98,6 +99,22 @@ class XMPPServiceTest {
         // THEN
         then(xmppClient).shouldHaveNoInteractions();
         then(webSocketTextMessageTransmitter).should().send(session, createTextMessage(FORBIDDEN));
+    }
+
+    @Test
+    void shouldSendErrorMessageWhenConnectionIsNotPresent() {
+        // GIVEN
+        Session session = Mockito.mock(Session.class);
+        String hashedPassword = BCrypt.hashpw(PASSWORD, BCrypt.gensalt());
+        given(accountService.getAccount(USERNAME)).willReturn(Optional.of(new Account(USERNAME, hashedPassword)));
+        given(xmppClient.connect(USERNAME, PASSWORD)).willReturn(Optional.empty());
+
+        // WHEN
+        xmppService.startSession(session, USERNAME, PASSWORD);
+
+        // THEN
+        then(xmppClient).shouldHaveNoMoreInteractions();
+        then(webSocketTextMessageTransmitter).should().send(session, createTextMessage(ERROR));
     }
 
     private TextMessage createTextMessage(MessageType type) {
