11468d1964ce448bafed3faadb499a69e99fdf86
Sergio Martin Rubio <econsergio@gmail.com>
2021-05-27 20:00:50 +0200

    Fix react join

--- reactjs-websocket-client/src/components/Join.js
+++ reactjs-websocket-client/src/components/Join.js
@@ -1,31 +1,36 @@
-import React, { useState } from 'react';
-import { Button, FormGroup, FormControl, FormLabel } from 'react-bootstrap';
-import { join } from '../store/actions/joinActions';
+import React, { useState } from "react";
+import { Button, FormControl, FormGroup, FormLabel } from "react-bootstrap";
+import { history } from "../browserhistory";
+import { wsConnect } from "../store";
+import storage from "../utils/storage";
 
 // https://www.igniterealtime.org/projects/openfire/plugins/1.2.1/websocket/readme.html
 export const Join = ({ dispatch }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const validateForm = () => {
     return username.length > 0 && password.length > 0;
   };
 
-  const handleSubmit = e => {
+  const connectAndJoin = (e) => {
     e.preventDefault();
-    dispatch(join(username));
+    dispatch(wsConnect(username));
+    storage.set("isAuthenticated", true);
+    storage.set("user", username);
+    history.push("/chat");
   };
 
   return (
     <div className="Join">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={connectAndJoin}>
         <FormGroup controlId="text">
           <FormLabel>Username</FormLabel>
           <FormControl
             autoFocus
             type="text"
             value={username}
-            onChange={e => setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </FormGroup>
         <FormGroup controlId="password">
@@ -34,7 +39,7 @@ export const Join = ({ dispatch }) => {
             autoFocus
             type="password"
             value={password}
-            onChange={e => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </FormGroup>
         <Button block disabled={!validateForm()} type="submit">
--- reactjs-websocket-client/src/store/actions/joinActions.js
+++
@@ -1,9 +0,0 @@
-import storage from '../../utils/storage';
-import { history } from '../../browserhistory';
-
-export const join = username => {
-  storage.set('isJoined', true);
-  storage.set('user', username);
-  history.push('/chat');
-  // history.go(0);
-};
